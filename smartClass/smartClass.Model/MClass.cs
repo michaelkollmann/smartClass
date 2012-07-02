621e1fa5a4be596590af06cd10b32ca1b71c784f
Michael Kollmann <me@michaelkollmann.de>
2012-07-02 16:26:26 +0200

    grundstruktur model erstellt
    IBase fertig
    MClass fertig

---
+++ smartClass/smartClass.Model/MClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smartClass.Model
+{
+    public class MClass : IBase
+    {
+        private int _id;
+        private string _name;
+
+        public MClass()
+            : this(Name : string.Empty, ID : -1) { }
+        public MClass(string Name, int ID)
+        {
+            this.Name = Name;
+            this.ID = ID;
+        }
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+        #endregion
+    }
+}
